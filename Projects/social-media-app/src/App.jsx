74c67ab31512935d40b9b968befb4ce4c237919e
pritam khaplee <pritamkhaple8585@gmail.com>
2024-03-14 22:01:45 +0530

    social ui first step UI commit

--- Projects/social-media-app/src/App.jsx
+++ Projects/social-media-app/src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Sidebar } from "./components/Sidebar";
+import { CreatePost } from "./components/CreatePost";
+import { Post } from "./components/Post";
 
 function App() {
-
   return (
     <>
-      <div>
-        <h2>hello</h2>
+      <div className="app-container">
+        <Sidebar></Sidebar>
+        <div className="content">
+          <Header></Header>
+          <CreatePost></CreatePost>
+          <Post></Post>
+          <Footer></Footer>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
