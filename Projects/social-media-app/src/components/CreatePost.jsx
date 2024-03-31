5bb830e2caf2568d61f9f63219535c2412c20925
pritam khaplee <pritamkhaple8585@gmail.com>
2024-03-31 13:25:16 +0530

    final social media app complete

--- Projects/social-media-app/src/components/CreatePost.jsx
+++ Projects/social-media-app/src/components/CreatePost.jsx
@@ -1,20 +1,99 @@
+import { useContext, useRef } from "react";
+import { PostList } from "../store/post-list-store";
+
 export const CreatePost = () => {
-    return(
-        <form>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-    )
-}
+  const { addPost } = useContext(PostList);
+
+  const  userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const reactionsElement = useRef();
+  const tagsElement = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userId = userIdElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    const reactions = reactionsElement.current.value
+    const tags = tagsElement.current.value.split(" ")
+
+    userIdElement.current.value = "";
+    postTitleElement.current.value = "";
+    postBodyElement.current.value = "";
+    reactionsElement.current.value = "";
+    tagsElement.current.value = "";
+
+    addPost(userId, postTitle, postBody, reactions, tags)
+  }
+
+  return (
+    <form className="create-post" onSubmit={handleSubmit}>
+
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          Your user id
+        </label>
+        <input
+          type="text"
+          ref={userIdElement}
+          className="form-control"
+          id="userId" 
+          placeholder="Your userId"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          ref={postTitleElement}
+          className="form-control"
+          id="title" 
+          placeholder="How are you feeling today?? ;)"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          ref={postBodyElement}
+          rows="4"
+          className="form-control"
+          id="body" 
+          placeholder="Tell us more about it!!!!"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Post reactions
+        </label>
+        <input
+          type="text"
+          ref={reactionsElement}
+          className="form-control"
+          id="reactions" 
+          placeholder="How many people reacted"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Post tags
+        </label>
+        <input
+          type="text"
+          ref={tagsElement}
+          className="form-control"
+          id="tags" 
+          placeholder="Enter ur tags"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </form>
+  );
+};
--- Projects/social-media-app/src/store/post-list-store.jsx
+++ Projects/social-media-app/src/store/post-list-store.jsx
@@ -7,24 +7,44 @@ export const PostList = createContext({
 });
 
 const postListReducer = (currPostList, action) => {
-  let newPostList = currPostList
-  if (action.type === 'DELETE_POST'){
-    newPostList = currPostList.filter((post) => post.id !== action.payload.postId)
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  }
+  else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
   }
   return newPostList;
 };
 
 export const PostListProvider = ({ children }) => {
-  const addPost = () => {};
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: 'ADD_POST',
+      payload: { 
+        id : Date.now(),
+        userId : userId, 
+        title : postTitle, 
+        body : postBody, 
+        reactions : reactions, 
+        tags : tags 
+      },
+    })
+  };
 
   const deletePost = (postId) => {
     dispatchPostList({
       type: "DELETE_POST",
-      payload: {postId},
-    })
+      payload: { postId },
+    });
   };
 
-  const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
 
   return (
     <PostList.Provider value={{ postList, addPost, deletePost }}>
